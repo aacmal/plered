64db6080fb6044701fbafbc7b019a11a79eb7e05
Aca M <acamaulana123@gmail.com>
2024-03-19 17:08:55 +0700

    Initial commit

---
+++ .prettierrc.mjs
@@ -0,0 +1,18 @@
+/** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions & import('@trivago/prettier-plugin-sort-imports').PluginConfig} */
+export default {
+  plugins: [
+    "@trivago/prettier-plugin-sort-imports",
+    "prettier-plugin-tailwindcss",
+  ],
+  semi: true,
+  tailwindFunctions: ["cn", "cva"],
+  importOrder: [
+    "<THIRD_PARTY_MODULES>",
+    "^@components/(.*)$",
+    "^@libs/(.*)$",
+    "^[./]",
+  ],
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
+  tabWidth: 2,
+};
